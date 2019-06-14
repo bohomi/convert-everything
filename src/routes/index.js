a5de7b18a869185a57a71578afa8f5e2bad17f47
bohomi <alina.draganescu@gmail.com>
2019-06-14 14:18:26 +0100

    project init

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import Home from "../components/Home";
+
+export default class Routes extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    );
+  }
+}
